4c8eaafeed45fe63e242a4a32c1ac032c26dd63c
ZachSNelson <zachnelson97@yahoo.com>
2017-09-22 14:54:05 -0600

    Created a Blank Deck class
    I need to make a list of cards (Deck). Set valuees of the cards

---
+++ Deck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Game
+{
+    public class Deck
+    {
+        
+
+    } // end deck class
+}
