7054a9bb99750b2843e7c871f08ab0b733b89e56
ZachSNelson <zachnelson97@yahoo.com>
2017-09-22 14:52:09 -0600

    Cards Class Created
    Defines the three parts of a card

---
+++ Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Game
+{
+    public class Card
+    {
+        // Card class defines all parts of a card
+        public string suit { get; set; }
+        public int value { get; set; }
+        public int card_number { get; set; }
+
+        public Card()
+        {
+            value = 0;
+            suit = "";
+            card_number = 0;
+        }
+
+    }// end class Card
+}
