ad1623db8cb3d2fdc139f21968ea046bfa150733
Zach Nelson <zachnelson97@yahoo.com>
2017-09-22 15:28:16 -0600

    Card Picker Class Created
    The card Picker class is a random number generator that picks a random number (card) to deal out.

---
+++ CardPicker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Game
+{
+    public class CardPicker
+    {
+        // randomly pick cards from the deck
+
+        public static int NumberBetween(int minimum, int maximum)
+        {
+            Random rnd = new Random();
+            int randomValue;
+
+            randomValue = rnd.Next(minimum, maximum);
+
+            // adds minvalue to randomvalue(0 to max)
+            return (minimum + randomValue); 
+        }
+
+    } // end CardPicker class
+}
